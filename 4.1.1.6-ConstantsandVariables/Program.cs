cd749b6323031724d5215d6aad491f846c2281f3
profs <profs@CHRONOS>
2023-09-18 10:28:04 +0100

    Add project files.

---
+++ 4.1.1.6-ConstantsandVariables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1._1._6_ConstantsandVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const bool hulkAnger = true;
+            //  hulkAnger = false; can not change
+            bool hulkForm = false;
+            hulkForm = true; // Can change
+            // Use constants when you don't want anything in the program to change a value.
+
+        }
+    }
+}
